f273b63c6a7e08964f330dc6936f6c3de9fa1ec2
James Engleback <jamesengleback@hotmail.co.uk>
2020-05-27 16:01:25 +0100

    cleared up argparse

--- testing/pipeline1/FoldMethodScreen.py
+++ testing/pipeline1/FoldMethodScreen.py
@@ -118,6 +118,6 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', name = 'fold')
+    parser.add_argument('-f', '--fold')
     args = parser.parse_args()
     main(args)

3b85349f475b2dfc46339ff19ec0da00b34b7724
James Engleback <jamesengleback@hotmail.co.uk>
2020-06-22 11:01:17 +0100

    moved files

--- setup.sh
+++ setup.sh
@@ -1,11 +1,4 @@
 conda create -n enz -y
+conda init bash
 conda activate enz
-conda install -c anaconda pip -y
-conda install pyrosetta -c  https://levinthal:paradox@conda.graylab.jhu.edu -y
-conda install -c https://conda.anaconda.org/biocore scikit-bio -y
-conda install -c oddt oddt -y
-conda install rdkit -c rdkit
-autodock-vina - docking
-conda install -c bioconda autodock-vina
-biopandas - cleaning pdbs
-conda install -c conda-forge biopandas
+conda install pip pyrosetta oddt rdkit autodock-vina biopandas scikit-bio -c  https://levinthal:paradox@conda.graylab.jhu.edu -c https://conda.anaconda.org/biocore -c scikit-bio -c oddt -c rdkit -c bioconda -c conda-forge -c anaconda

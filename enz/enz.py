69cc7e5975642da244a0a2f81b9d6720de28c6ea
james <jamesengleback@hotmail.co.uk>
2020-11-06 14:59:35 +0000

    almost done with this dev, going to make a mol base class to make it easier to view docking results as a df

--- enz/enz.py
+++ enz/enz.py
@@ -1,178 +1,196 @@
+import tempfile
 import os
 import shutil
-import tempfile
-from string import ascii_lowercase, ascii_uppercase
-import random
-from multiprocessing import cpu_count
+from distutils.spawn import find_executable
+import subprocess
+import re
 
 import pandas as pd
+
 from biopandas.pdb import PandasPdb
+import nwalign3 as nw
+from openbabel import pybel
 
 from pyrosetta import pose_from_pdb
 from pyrosetta import init as pyrosetta_init
 from pyrosetta.toolbox import mutate_residue
 
-from skbio.alignment import global_pairwise_align_protein
-from  skbio import Protein as skbioProtein
-
-import oddt
-from oddt import docking
-
 PYROSETTA_INIT = False
 
-def uniqueID():
-    return ''.join(random.sample(ascii_lowercase + ascii_uppercase, 20))
-class protein:
-    '''
-    '''
-    def __init__(self,
-                pdb = None,
-                seq = None,
-                key_sites = {},
-                chain=None,
-                cofactors = []):
-        # TODO: select chain
-        self.PDB = os.path.abspath(pdb)
-        self.ID = uniqueID()
-        self.CACHE = tempfile.mkdtemp(prefix='enzp-')
-        self.STRUCTURE =  os.path.join(self.CACHE, self.ID + '.pdb') # path
-        self.COFACTORS = cofactors
-        self._cleanPDB() # save to self.STRUCTURE
-        self.seq = self.PDBSEQ if seq == None else seq # self.pdbSeq set in self._cleanPDB
-        self.KEY_SITES = {i:self.seq[i] for i in key_sites}
+class mol:
+    def __init__(self, path):
+        pass
     @property
     def df(self):
-        return pd.concat([PandasPdb().read_pdb(self.STRUCTURE).df[i] for i in ['ATOM','HETATM']]).reset_index(drop=True)
+        pass
+    def save(self, path):
+        pass
+
+class protein:
+    # front end
+    def __init__(self, pdb_path, seq = None, cofactors = [], key_sites = []):
+        self.pdb_path = pdb_path
+        self.CACHE = tempfile.mkdtemp()
+        self.key_sites = key_sites
+        self.cofactors = cofactors
+        self.struc = pdb_fns.clean_pdb(pdb_path = self.pdb_path,
+                    save_path = os.path.join(self.CACHE, 'clean.pdb'),
+                    cofactors = self.cofactors)
+        self.pdb_seq = pdb_fns.get_seq(self.struc)
+        self.seq = self.pdb_seq if seq == None else seq
     @property
-    def KEY_SITES_DICT(self):
-        if self.KEY_SITES != None:
-            return {i:self.seq[i] for i in self.KEY_SITES}
-    @property
-    def docking_results(self):
-        if hasattr(self, 'vina'):
-            return self.vina.CACHE
-    def _cleanPDB(self):
-        data = PandasPdb().read_pdb(self.PDB)
-        atoms = data.df['ATOM']
-        hetatms = data.df['HETATM']
-        firstChain = atoms['chain_id'].unique()[0]
-        atoms = atoms.loc[atoms['chain_id'] == firstChain,:]
-        hetatms = hetatms.loc[hetatms['residue_name'] != 'HOH',:].loc[hetatms['chain_id'] == firstChain,:]
-        print('HETATMs:')
-        for i in hetatms['residue_name'].unique():
-            included = 'included' if i in self.COFACTORS else 'not included'
-            print(f'{i} - ({included})')
-        if len(self.COFACTORS) > 0:
-            hetatms= pd.concat([hetatms.loc[hetatms['residue_name'] == i,:].loc[hetatms['chain_id'] == firstChain,:] for i in self.COFACTORS])
-        data.df['HETATM'] = hetatms
-        data.df['ATOM'] = atoms
-        data.to_pdb(self.STRUCTURE)
-        self.PDBSEQ = ''.join([i for i in data.amino3to1()['residue_name']]) # PDBSEQ
-    def mutate(self, idx, aa):
+    def df(self):
+        pass
+    def mutate(self, position, aa):
         seq = list(self.seq)
-        seq[idx] = aa
+        seq[position] = aa
         self.seq = ''.join(seq)
     def refold(self):
-        aln1, aln2 = aln(self.seq, self.PDBSEQ)
+        aln1, aln2 = aln(self.pdb_seq, self.seq)
         mutations = diff(aln1, aln2)
+        global PYROSETTA_INIT
         if  not PYROSETTA_INIT:
             pyrosetta_init(silent=True)
             PYROSETTA_INIT = True
-        self.pose = pose_from_pdb(self.STRUCTURE)
+        self.pose = pose_from_pdb(self.struc)
         for i in mutations:
             mutate_residue(self.pose, i, mutations[i]['to'], pack_radius = 5.0)
-        self.pose.dump_pdb(self.STRUCTURE)
+        self.pose.dump_pdb(self.struc)
     def save(self, path):
-        shutil.copy(self.STRUCTURE, path)
-    def dock(self,smiles, name=None):
-        if not hasattr(self, 'vina'):
-            self.vina = vina(pdb = self.STRUCTURE,
-                            key_sites = self.KEY_SITES,)
-        scores = self.vina.dock(smiles,name)
-        return scores
-    def save_docking_results(self,path):
-        if hasattr(self,'vina'):
-            self.vina.save(path)
+        # copy self.stuc -> path
+        shutil.copyfile(self.struc, path)
+    def dock(self, smiles, save_path = None):
+        df = vina.dock(self.struc,
+                    smiles,
+                    save_path = save_path,
+                    cofactors = self.cofactors,
+                    target_residues = self.key_sites)
+        return df
 
+class pdb_fns:
+    def clean_pdb(pdb_path, save_path, cofactors = [], chain_selection = 'A'):
+        structure = PandasPdb().read_pdb(pdb_path)
+        atoms = structure.df['ATOM'].copy()
+        hetatms = structure.df['HETATM'].copy()
+        atoms = atoms.loc[atoms['chain_id'] == chain_selection,:]
+        hetatms = hetatms.loc[hetatms['chain_id'] == chain_selection,:]
+        het_garbage = [i for i in hetatms['residue_name'].unique() if i not in cofactors]
+        hetatms = hetatms.loc[hetatms['residue_name'].isin(het_garbage) == False,:]
+        structure.df['ATOM'] = atoms
+        structure.df['HETATM'] = hetatms
+        structure.to_pdb(save_path)
+        return save_path
+    def get_seq(pdb_path):
+        structure = PandasPdb().read_pdb(pdb_path)
+        sequences = structure.amino3to1() # cols = ['chain_id', 'residue_name']
+        seqs = [''.join(sequences.loc[sequences['chain_id'] == i,'residue_name'].to_list()) for i in sequences['chain_id'].unique()]
+        return seqs[0] if len(seqs) == 1 else seqs
+    def draw_box(pdb_path, key_sites):
+        receptor = PandasPdb().read_pdb(pdb_path)
+        df = receptor.df['ATOM']
+        target_site = df.loc[df['residue_number'].isin(key_sites),:]
+        coords = target_site.loc[:,['x_coord','y_coord','z_coord']]
+        center = coords.mean(axis=0)
+        sizes = (coords.max(axis=0) - coords.min(axis=0)) * 1.2
+        box = {'--center_x':center['x_coord'],
+                '--center_y':center['y_coord'],
+                '--center_z':center['z_coord'],
+                '--size_x':sizes['x_coord'],
+                '--size_y':sizes['y_coord'],
+                '--size_z':sizes['z_coord']}
+        return box
 
+class obabel_fns:
+    def pdb_to_pdbqt(pdb, save_path):
+        m = list(pybel.readfile('pdb',pdb))
+        assert len(m) == 1
+        m = m[0]
+        m.addh()
+        m.write('pdbqt', save_path, opt={'r':True}, overwrite=True) # opt:r = rigid - less errors?? - revisit this
+        return save_path
+    def smiles_to_pdbqt(smiles, save_path):
+        m = pybel.readstring('smi',smiles)
+        m.addh()
+        m.make3D()
+        m.write('pdbqt',save_path)
+        return save_path
 
-class vina():
-    def __init__(self,
-                 pdb,
-                 key_sites=None,
-                 center = None,
-                 box_dims = None,
-                 ncpus=cpu_count() -1,
-                 exhaustiveness=8):
-        self.PDB = os.path.abspath(pdb)
-        self.ID = uniqueID()
-        self.CACHE = tempfile.mkdtemp(prefix='enzv-') #os.path.join('__enz__', self.ID) # tempfile.mkdtemp
-        self.STRUCTURE =  os.path.join(self.CACHE, self.ID + '.pdb') # path
-        self.KEY_SITES = key_sites
-        self.ncpus = ncpus
-        self.exhaustiveness = exhaustiveness
-        self._cleanPDB() # problem for heterodimers
-        self.vina = self._init_vina()
-    @property
-    def df(self):
-        cached_files = [os.path.join(parent,file) for parent, _, files in os.walk(self.CACHE) for file in files]
-        return pd.concat([PandasPdb().read_pdb(j).df[i] for i in ['ATOM','HETATM'] for j in cached_files])
-    def _cleanPDB(self): # slow?
-        data = PandasPdb().read_pdb(self.PDB)
-        atoms = data.df['ATOM']
-        hetatms = data.df['HETATM']
-        firstChain = atoms['chain_id'].unique()[0]
-        data.df['ATOM'] = atoms.loc[atoms['chain_id'] == firstChain,:]
-        data.df['HETATM'] = hetatms.loc[hetatms['residue_name'] != 'HOH',:].loc[hetatms['chain_id'] == firstChain,:]
-        data.to_pdb(self.STRUCTURE)
-    def _init_vina(self):
-        if self.KEY_SITES != None:
-            center, box_dims = self._boxDims()
-        else:
-            center, box_dims = (0,0,0), (20,20,20)
-        vina = docking.autodock_vina(self._read_pdb(self.STRUCTURE),
-                                                n_cpu = self.ncpus,
-                                                exhaustiveness=self.exhaustiveness,
-                                                size=box_dims,
-                                                center=center)
-        return vina
-    def _read_pdb(self,path):
-        oddt_protein = next(oddt.toolkit.readfile('pdb', path))
-        oddt_protein.protein = True # register that it's a protein
-        oddt_protein.addh()
-        return oddt_protein
-    def _read_smiles(self,smiles): # todo: move to tools
-        mol = oddt.toolkit.readstring('smi',smiles)
-        mol.addh()
-        mol.make3D()
-        return mol
-    def _boxDims(self):
-        df = self.df
-        key_sites_coords = df.loc[df['residue_number'].isin(self.KEY_SITES), ['x_coord','y_coord','z_coord']]
-        center = key_sites_coords.mean(axis=0)
-        x,y,z = [(key_sites_coords[i].max() - key_sites_coords[i].min()) * 1.2 for i in key_sites_coords]
-        return center, (x,y,z)
+class vina:
+    def dock(receptor_pdb,
+            smiles,
+            save_path = None,
+            cofactors = [],
+            target_residues = [],
+            exhaustiveness=8,
+            vina_executable = find_executable('vina'),
+            vina_split_executable = find_executable('vina_split')):
+        CACHE = tempfile.mkdtemp()
+        raw_vina_results = os.path.join(CACHE, 'vina.result')
+        # todo : if not clean
+        clean_receptor_pdb = pdb_fns.clean_pdb(receptor_pdb, os.path.join(CACHE, f'{os.path.basename(receptor_pdb)}.clean'), cofactors = cofactors)
+        receptor_pdbqt = obabel_fns.pdb_to_pdbqt(clean_receptor_pdb, os.path.join(CACHE,'receptor.pdbqt'))
+        ligand_pdbqt = obabel_fns.smiles_to_pdbqt(smiles, os.path.join(CACHE,'ligand.pdbqt'))
+        args = {'--receptor':receptor_pdbqt,
+                    '--ligand':ligand_pdbqt,
+                    '--out':raw_vina_results,
+                    '--exhaustiveness':exhaustiveness}
+        args.update(pdb_fns.draw_box(clean_receptor_pdb, target_residues))
+        args_list_vina = [vina_executable]
+        for i in args:
+            args_list_vina.append(i)
+            args_list_vina.append(str(args[i]))
+        # execute
+        p1 = subprocess.check_output(args_list_vina)
+        docking_scores = vina.extract_scores(p1.decode())
+        if save_path != None:
+            vina.split(raw_vina_results,
+                        save_path,
+                        vina_split_executable)
+        return docking_scores
 
-    def dock(self, smiles, name = None):
-        poses = self.vina.dock(self._read_smiles(smiles))
-        scores = pd.concat([pd.Series(i.data) for i in self.vina.score(poses,
-                        self._read_pdb(self.STRUCTURE))], axis=1, join='outer').T # ValueError: No objects to concatenate
-        newIdx = []
-        for i,(pose,score) in enumerate(zip(poses, scores['vina_affinity'])):
-            savepath = os.path.join(self.CACHE, f'pose:aff:{score}-{uniqueID()}.pdb')
-            pose.write('pdb', savepath)
-            newIdx.append(savepath)
-        scores['path'] = newIdx
-        scores.to_csv(os.path.join(self.CACHE, 'vinaScores.csv'))
-        return scores
-    def save(self, path):
-        shutil.copytree(self.CACHE, path)
+    def split(vina_output, save_path, vina_split_executable):
+        # vina_split
+        args_list_vina_split = [vina_split_executable, '--input', outpath]
+        p2 = subprocess.Popen(args_list_vina_split, stdout=subprocess.DEVNULL)
+        p2.wait()
+        # convert pdbqt files
+        docking_poses_pdbqt = [os.path.join(CACHE,i) for i in os.listdir(CACHE) if 'vina_ligand_' in i]
+        docking_poses_pdb = [save_pose(i, os.path.join(CACHE, f'pose_{j}.pdb')) for j,i in enumerate(docking_poses_pdbqt)]
+        # move to savepath
+        os.makedirs(save_path, exist_ok=True)
+        # remove existing contents
+        for i in os.listdir(save_path):
+            os.remove(os.path.join(save_path,i))
+        for i in docking_poses_pdb:
+            shutil.copyfile(i, os.path.join(save_path, os.path.basename(i))) # copy to inside savepath dir
+        shutil.copyfile(clean_receptor_pdb, os.path.join(save_path, os.path.basename(clean_receptor_pdb)))
+        docking_scores.to_csv(os.path.join(save_path, 'docking-scores.csv')) # and save scores
 
-def aln(s1, s2):
-    aln = global_pairwise_align_protein(skbioProtein(s1),skbioProtein(s2))[0]
-    aln_s1 = ''.join([i.decode() for i in aln[0].values])
-    aln_s2 = ''.join([i.decode() for i in aln.loc[1].values])
-    return aln_s1, aln_s2
+    def extract_scores(text):
+        # extract scores from vina output
+        text = text.split('\n')
+        table_start = ['---+--' in i for i in text].index(True) + 1
+        is_all_ints = lambda l : sum([re.search('-?\d+', i) is not None for i in l])
+        table = []
+        for row in text[table_start:]:
+            items = row.split()
+            if len(items) == 4 and is_all_ints(items):
+                table.append(dict(zip(['mode','affinity (kcal/mol)', 'dist from best mode - rmsd - ub','dist from best mode - lb'], items)))
+        return pd.DataFrame(table)
+
+def aln(s1,s2):
+    return nw.global_align(s1,s2)
 
 def diff(s1,s2):
     return {i:{'from':x, 'to':y} for i, (x,y) in enumerate(zip(s1,s2)) if x != y and x != '-' and y != '-'}
+
+def test():
+    path = '../test/4KEY.pdb'
+    p = protein(path, cofactors = 'HEM', key_sites = [87,82,400,330,263,188,49,51])
+    p.mutate(70,'A')
+    p.refold()
+    p.save('test.pdb')
+    print(p.dock('CCCCCCCCCCCC=O', 'results'))
+
+if __name__ == '__main__':
+    test()

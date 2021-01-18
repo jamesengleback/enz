6da96549131f232d8244af7d333fc8e6e47e5b12
james <jamesengleback@hotmail.co.uk>
2021-01-18 16:42:14 +0000

    deleted some stuff, just arrived from another job and want to switch branches to some ion / atom type filtering

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name = 'enz',
-version=0.15,
+setup(name = 'enz-jax',
+version=0.01,
 description='enzyme design',
 author='james engleback',
 packages = find_packages())
